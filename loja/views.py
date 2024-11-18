7c239ed60198d0955d9c1d8fe644a019ceab8ffe
emerson <emersoneletrotecnico2013@hotmail.com>
2024-11-18 00:28:07 -0300

    eccomerce
    parte 3 do projeto

--- loja/views.py
+++ loja/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-def Produc_detail(request, slug):
-    return render(request, 'loja/produtos_detalhes.html')
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = Category.products.all()
+    return render(request, 'loja/category_detalhes.html', {'category': category, 'products': products})
+
+def Product_detail(request, category_slug, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'loja/produtos_detalhes.html', {'product': product})
