a1da6fd0d1ced669a8d0626bac1e5a1fe55d4d6c
emerson <emersoneletrotecnico2013@hotmail.com>
2024-11-19 08:53:00 -0300

    ecommerce
    atualização

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('userprofile.urls')),
     path('', include('ecommerce.urls')),
     path('', include('loja.urls')),
-    path('', include('userprofile.urls')),
+   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('venders/<int:pk>/', views.vendor_details, name='vendor_details')
+    path('vendors/<int:pk>/', views.vendor_details, name='vendor_details')
 ]
